348dd4f24f42f74b252f98feebcda94539e3aaf8
werdnanoslen <andy@andyhub.com>
2014-02-25 12:39:19 +0100

    Laid out category pages

--- content.php
+++ content.php
@@ -5,31 +5,21 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <?php // Don't display excerpt_image for Search
+        if (!is_search()) {
+            echo get_post_custom_values('excerpt_image')[0];
+        }
+    ?>
+    
 	<header class="entry-header">
-		<h2 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-
-		<?php if ( 'post' == get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php andyhub_wp_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
+		<h2 class="entry-title">
+            <a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
+        </h2>
 	</header><!-- .entry-header -->
 
-	<?php if ( is_search() ) : // Only display Excerpts for Search ?>
-	<div class="entry-summary">
+	<div class="entry-<?php echo (is_search() ? "summary" : "content") ?>">
 		<?php the_excerpt(); ?>
-	</div><!-- .entry-summary -->
-	<?php else : ?>
-	<div class="entry-content">
-		<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'andyhub_wp' ) ); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'andyhub_wp' ),
-				'after'  => '</div>',
-			) );
-		?>
 	</div><!-- .entry-content -->
-	<?php endif; ?>
 
 	<footer class="entry-meta">
 		<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
