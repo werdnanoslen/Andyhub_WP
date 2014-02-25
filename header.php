606c1f9b5e5d65c3cfa656cdb6f9cdaff08b47bc
werdnanoslen <andy@andyhub.com>
2014-02-25 10:58:20 +0100

    Finished building nav

--- header.php
+++ header.php
@@ -22,13 +22,13 @@
 <div id="page" class="hfeed site">
 
 	<header id="masthead" class="site-header" role="banner">
-        <h1 class="site-title">
+        <div class="site-title">
             <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
                 <?php bloginfo( 'name' ); ?>
             </a>
-        </h1>
+        </div>
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<h1 class="menu-toggle"><?php _e( 'Menu', 'andyhub_wp' ); ?></h1>
+			<h1 class="menu-toggle"><?php _e( '&#8803;', 'andyhub_wp' ); ?></h1>
 			<a class="skip-link screen-reader-text" href="#content">
                 <?php _e( 'Skip to content', 'andyhub_wp' ); ?>
             </a>
