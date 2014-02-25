ec520a9ce77f13d32c047a143930b128df5a5449
werdnanoslen <andy@andyhub.com>
2014-02-25 11:24:32 +0100

    removed footer and sidebar

--- footer.php
+++ footer.php
@@ -12,9 +12,6 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="http://wordpress.org/" rel="generator"><?php printf( __( 'Proudly powered by %s', 'andyhub_wp' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'andyhub_wp' ), 'Andyhub_WP', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
