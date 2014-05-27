0066de62226fe2e6da924c6c25ad9c1e214402f8
werdnanoslen <werdnanoslen@gmail.com>
2014-05-27 10:38:01 -0500

    Customized 404 page

--- 404.php
+++ 404.php
@@ -12,41 +12,19 @@ get_header(); ?>
 
 			<section class="error-404 not-found">
 				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'andyhub_wp' ); ?></h1>
+					<h1 class="page-title"><?php _e( 'That\'s a 404 good buddy.', 'andyhub_wp' ); ?></h1>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'andyhub_wp' ); ?></p>
-
 					<?php get_search_form(); ?>
-
-					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
-
-					<?php if ( andyhub_wp_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
-					<div class="widget widget_categories">
-						<h2 class="widgettitle"><?php _e( 'Most Used Categories', 'andyhub_wp' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-					<?php endif; ?>
-
-					<?php
-					/* translators: %1$s: smiley */
-					$archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', 'andyhub_wp' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-					?>
-
-					<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
-
+                    <br />
+                    <object type="application/x-shockwave-flash" data="http://thebest404pageever.com/swf/hey.swf" width="640" height="480" id="object_404">
+                        <param name="allowScriptAccess" value="sameDomain" />
+                        <param name="movie" value="http://thebest404pageever.com/swf/hey.swf" />
+                        <param name="menu" value="false" />
+                        <param name="quality" value="high" />
+                        <embed src="http://thebest404pageever.com/swf/hey.swf" menu="false" quality="high" width="640" height="480" id="embed_404" allowScriptAccess="sameDomain" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />
+                    </object>
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
