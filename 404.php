ac629fa1355c747b4358bd41d860bf0b5551dfd6
Andrew Nelson <andy@andyhub.com>
2015-08-29 23:21:15 -0400

    replaced flash with youtube embed

--- 404.php
+++ 404.php
@@ -17,14 +17,8 @@ get_header(); ?>
 
 				<div class="page-content">
 					<?php get_search_form(); ?>
-                    <br />
-                    <object type="application/x-shockwave-flash" data="http://thebest404pageever.com/swf/hey.swf" width="640" height="480" id="object_404">
-                        <param name="allowScriptAccess" value="sameDomain" />
-                        <param name="movie" value="http://thebest404pageever.com/swf/hey.swf" />
-                        <param name="menu" value="false" />
-                        <param name="quality" value="high" />
-                        <embed src="http://thebest404pageever.com/swf/hey.swf" menu="false" quality="high" width="640" height="480" id="embed_404" allowScriptAccess="sameDomain" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />
-                    </object>
+		                    	<br />
+		                    	<iframe width="640" height="480" src="https://www.youtube-nocookie.com/embed/ZZ5LpwO-An4?autoplay=1" frameborder="0" allowfullscreen></iframe>
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
