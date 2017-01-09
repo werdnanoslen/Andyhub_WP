f2c6a4c5849f1396743ec32ce506dc4fab01a2cb
Andrew Nelson <andy@andyhub.com>
2017-01-08 22:51:23 -0600

    removed comments link from content page

--- content.php
+++ content.php
@@ -15,7 +15,7 @@ if ( has_post_thumbnail() ) {
             <img src="<?php echo $img; ?>" alt="<?php the_title(); ?>" />
         </a>
     <?php endif; ?>
-    
+
     <?php if (!is_front_page()) : //Don't show title on Home page ?>
         <header class="entry-header">
             <h2 class="entry-title">
@@ -40,10 +40,6 @@ if ( has_post_thumbnail() ) {
 			</span>
 			<?php endif; // End if $tags_list ?>
 		<?php endif; // End if 'post' == get_post_type() ?>
-
-		<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
-		<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'andyhub_wp' ), __( '1 Comment', 'andyhub_wp' ), __( '% Comments', 'andyhub_wp' ) ); ?></span>
-		<?php endif; ?>
 
 		<?php edit_post_link( __( 'Edit', 'andyhub_wp' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-meta -->
