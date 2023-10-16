d4729bcbe6e369bc1f6d264bec921d9eb8c97306
Andrew Nelson <andy@andyhub.com>
2023-10-16 13:54:37 -0700

    combine link elements

--- front-page.php
+++ front-page.php
@@ -33,32 +33,32 @@ get_header(); ?>
             ?>
                 <h1><a href="/contact">Contact me</a> about...</h1>
                 <?php foreach ( $posts_array as $post ) : setup_postdata( $post ); ?>
-                        <article id="post-<?php the_ID(); ?>">
-                            <a href="<?php the_permalink(); ?>" rel="bookmark">
-                                <?php
-				if ( has_post_thumbnail() ) {
-       					$img = get_the_post_thumbnail_url(get_the_ID(), 'medium');
-					$alt = get_post_meta(get_post_thumbnail_id(), '_wp_attachment_image_alt', TRUE);
-				} else if ( get_post_custom_values('excerpt_image')[0] ) {
-      					$img = get_post_custom_values('excerpt_image')[0];
-				} else {
-				        $bgColor = "".substr(md5(rand()), 0, 6);
-				        $r = hexdec(substr($bgColor,0,2));
-				        $g = hexdec(substr($bgColor,2,2));
-       					$b = hexdec(substr($bgColor,4,2));
-				        $fgColor = ($r + $g + $b > 400) ? 40 : 'F';
-				        $img = "https://dummyimage.com/300x300/$bgColor/$fgColor/&text=" . get_the_title();
-				}
-				?>
-				<img src="<?php echo $img ?>" alt="<?php echo $alt ?>" />
-                            </a>
-
+                    <article id="post-<?php the_ID(); ?>">
+                        <a href="<?php the_permalink(); ?>" rel="bookmark">
                             <header class="entry-header">
                                 <h2 class="entry-title">
-                                    <a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
-                                </h2>
-                            </header><!-- .entry-hfeader -->
-                        </article><!-- #post-## -->
+                                    <a href="<?php the_permalink(); ?>" rel="bookmark">
+                                    <?php
+                                        if ( has_post_thumbnail() ) {
+                                                $img = get_the_post_thumbnail_url(get_the_ID(), 'medium');
+                                            $alt = get_post_meta(get_post_thumbnail_id(), '_wp_attachment_image_alt', TRUE);
+                                        } else if ( get_post_custom_values('excerpt_image')[0] ) {
+                                                $img = get_post_custom_values('excerpt_image')[0];
+                                        } else {
+                                                $bgColor = "".substr(md5(rand()), 0, 6);
+                                                $r = hexdec(substr($bgColor,0,2));
+                                                $g = hexdec(substr($bgColor,2,2));
+                                                $b = hexdec(substr($bgColor,4,2));
+                                                $fgColor = ($r + $g + $b > 400) ? 40 : 'F';
+                                                $img = "https://dummyimage.com/300x300/$bgColor/$fgColor/&text=" . get_the_title();
+                                        }
+                                    ?>
+                                    <img src="<?php echo $img ?>" alt="<?php echo $alt ?>" />
+                                    <?php the_title(); ?>
+                                </a>
+                            </h2>
+                        </header><!-- .entry-hfeader -->
+                    </article><!-- #post-## -->
                 <?php endforeach;
                     wp_reset_postdata();
                 ?>
