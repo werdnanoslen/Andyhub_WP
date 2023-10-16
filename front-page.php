b5b2de5d3d368f34448281805c9d5b30e6ef1a37
Andrew Nelson <andy@andyhub.com>
2023-10-16 14:22:35 -0700

    merge (#3)

--- front-page.php
+++ front-page.php
@@ -25,42 +25,37 @@ get_header(); ?>
         <main class="site-main" role="main">
             <?php
                 $args = array(
-                    'category_name'    => 'Featured',
-                    'post_status'      => 'publish',
-                    'posts_per_page'   => -1,
-                    'suppress_filters' => true
+                    'meta_key' => '_wp_page_template',
+                    'meta_value' => 'wide-page.php'
                 );
-                $posts_array = get_posts($args);
+                $posts_array = get_pages($args); 
                 if (count($posts_array) > 0) :
             ?>
-                <h1>Featured work</h1>
+                <h2><a href="/contact">Contact me</a> about...</h2>
                 <?php foreach ( $posts_array as $post ) : setup_postdata( $post ); ?>
-                        <article id="post-<?php the_ID(); ?>">
-                            <a href="<?php the_permalink(); ?>" rel="bookmark">
-                                <?php
-				if ( has_post_thumbnail() ) {
-       					$img = get_the_post_thumbnail_url(get_the_ID(), 'medium');
-					$alt = get_post_meta(get_post_thumbnail_id(), '_wp_attachment_image_alt', TRUE);
-				} else if ( get_post_custom_values('excerpt_image')[0] ) {
-      					$img = get_post_custom_values('excerpt_image')[0];
-				} else {
-				        $bgColor = "".substr(md5(rand()), 0, 6);
-				        $r = hexdec(substr($bgColor,0,2));
-				        $g = hexdec(substr($bgColor,2,2));
-       					$b = hexdec(substr($bgColor,4,2));
-				        $fgColor = ($r + $g + $b > 400) ? 40 : 'F';
-				        $img = "https://dummyimage.com/300x300/$bgColor/$fgColor/&text=" . get_the_title();
-				}
-				?>
-				<img src="<?php echo $img ?>" alt="<?php echo $alt ?>" />
-                            </a>
-
+                    <article id="post-<?php the_ID(); ?>">
+                        <a href="<?php the_permalink(); ?>" rel="bookmark">
                             <header class="entry-header">
-                                <h2 class="entry-title">
-                                    <a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
-                                </h2>
+                                <?php
+                                    if ( has_post_thumbnail() ) {
+                                        $img = get_the_post_thumbnail_url(get_the_ID(), 'medium');
+                                        $alt = get_post_meta(get_post_thumbnail_id(), '_wp_attachment_image_alt', TRUE);
+                                    } else if ( get_post_custom_values('excerpt_image')[0] ) {
+                                        $img = get_post_custom_values('excerpt_image')[0];
+                                    } else {
+                                        $bgColor = "".substr(md5(rand()), 0, 6);
+                                        $r = hexdec(substr($bgColor,0,2));
+                                        $g = hexdec(substr($bgColor,2,2));
+                                        $b = hexdec(substr($bgColor,4,2));
+                                        $fgColor = ($r + $g + $b > 400) ? 40 : 'F';
+                                        $img = "https://dummyimage.com/300x300/$bgColor/$fgColor/&text=" . get_the_title();
+                                    }
+                                ?>
+                                <img src="<?php echo $img ?>" alt="<?php echo $alt ?>" />
+                                <h3 class="entry-title"><?php the_title(); ?></h3>
                             </header><!-- .entry-hfeader -->
-                        </article><!-- #post-## -->
+                        </a>
+                    </article><!-- #post-## -->
                 <?php endforeach;
                     wp_reset_postdata();
                 ?>
