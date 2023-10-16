723b71796b4499ad7df471891daa317915cdd56d
Andrew Nelson <andy@andyhub.com>
2023-10-16 14:03:03 -0700

    fix mess

--- front-page.php
+++ front-page.php
@@ -36,26 +36,25 @@ get_header(); ?>
                     <article id="post-<?php the_ID(); ?>">
                         <a href="<?php the_permalink(); ?>" rel="bookmark">
                             <header class="entry-header">
+                                <?php
+                                    if ( has_post_thumbnail() ) {
+                                        $img = get_the_post_thumbnail_url(get_the_ID(), 'medium');
+                                        $alt = get_post_meta(get_post_thumbnail_id(), '_wp_attachment_image_alt', TRUE);
+                                    } else if ( get_post_custom_values('excerpt_image')[0] ) {
+                                        $img = get_post_custom_values('excerpt_image')[0];
+                                    } else {
+                                        $bgColor = "".substr(md5(rand()), 0, 6);
+                                        $r = hexdec(substr($bgColor,0,2));
+                                        $g = hexdec(substr($bgColor,2,2));
+                                        $b = hexdec(substr($bgColor,4,2));
+                                        $fgColor = ($r + $g + $b > 400) ? 40 : 'F';
+                                        $img = "https://dummyimage.com/300x300/$bgColor/$fgColor/&text=" . get_the_title();
+                                    }
+                                ?>
                                 <img src="<?php echo $img ?>" alt="<?php echo $alt ?>" />
-                                <h2 class="entry-title">
-                                    <?php
-                                        if ( has_post_thumbnail() ) {
-                                                $img = get_the_post_thumbnail_url(get_the_ID(), 'medium');
-                                            $alt = get_post_meta(get_post_thumbnail_id(), '_wp_attachment_image_alt', TRUE);
-                                        } else if ( get_post_custom_values('excerpt_image')[0] ) {
-                                                $img = get_post_custom_values('excerpt_image')[0];
-                                        } else {
-                                                $bgColor = "".substr(md5(rand()), 0, 6);
-                                                $r = hexdec(substr($bgColor,0,2));
-                                                $g = hexdec(substr($bgColor,2,2));
-                                                $b = hexdec(substr($bgColor,4,2));
-                                                $fgColor = ($r + $g + $b > 400) ? 40 : 'F';
-                                                $img = "https://dummyimage.com/300x300/$bgColor/$fgColor/&text=" . get_the_title();
-                                        }
-                                    ?>
-                                    <?php the_title(); ?>
-                                </h2>
-                        </header><!-- .entry-hfeader -->
+                                <h2 class="entry-title"><?php the_title(); ?></h2>
+                            </header><!-- .entry-hfeader -->
+                        </a>
                     </article><!-- #post-## -->
                 <?php endforeach;
                     wp_reset_postdata();
